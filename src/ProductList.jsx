34ec17205a6cdd7e5a86b609633652b98022a7e6
Haewon Jeon <haewonito@gmail.com>
2025-02-19 15:23:53 -0700

    just testing

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,7 +7,7 @@ function ProductList() {
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
   const [addButtonEnabled, setAddButtonEnabled] = useState(true);
-
+  // just putting some comments to check something
   const plantsArray = [
     {
       category: "Air Purifying Plants",
